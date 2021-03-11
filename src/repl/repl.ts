138d0bd3c9b417ad9a18cbfeee25915b882b7f1f
taogawa <taogwjp@gmail.com>
2021-03-12 07:28:43 +0900

    Extends REPL

--- src/repl/repl.ts
+++ src/repl/repl.ts
@@ -1,8 +1,8 @@
 import readline from 'readline';
-import { TokenTypes } from '../token/token';
 import { Lexer } from '../lexer/lexer';
+import { Parser } from '../parser/parser';
 
-export const startRepl = ():void => {
+export const startRepl = (): void => {
   const prompt = '>>';
   const rl = readline.createInterface({
     input: process.stdin,
@@ -11,12 +11,34 @@ export const startRepl = ():void => {
   });
   rl.on('line', (input) => {
     const l = new Lexer(input);
-    for (
-      let tok = l.nextToken();
-      tok.type !== TokenTypes.EOF;
-      tok = l.nextToken()
-    ) {
-      console.log(tok);
+    const p = new Parser(l);
+
+    const program = p.parseProgram();
+    if (p.errors.length != 0) {
+      printParserErrors(p.errors);
     }
+    console.log(program.toString());
+  });
+};
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \\/  .-. .-.  \\/ .. \\
+ | |  '|  /   Y   \\  |'  | |
+ | \\   \\  \\ 0 | 0 /  /   / |
+  \\ '- ,\\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\\ '-''
+       |  \\._   _./  |
+       \\   \\ '~' /   /
+        '._ '-=-' _.'
+           '-----
+`;
+
+const printParserErrors = (errors: string[]): void => {
+  console.log(MONKEY_FACE);
+  console.log('Woops! We ran into some monkey business here!');
+  console.log(' parser errors:');
+  errors.forEach((msg) => {
+    console.log(`\t${msg}`);
   });
 };
